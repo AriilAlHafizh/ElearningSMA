0ba3b32e13f9233e5a561f034c96f2a651781f9c
AriilAlHafizh <ariil.alhafizh26@gmail.com>
2024-12-25 18:08:47 +0700

    Rev 1 frontend

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,7 +7,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="#">
   <link rel="icon" type="image/png" href="#">
   <title>
-    Login Page
+    Login Page Elearning
   </title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
